6098fa4261677e8d9ae1aba186535f5c2bbc13ef
hiroaki-1017 <yh111017@outlook.jp>
2021-02-05 01:13:24 +0900

    sakuzyo

--- index.js
+++ index.js
@@ -22,8 +22,9 @@ app.get('/api/v1/list', (req, res) => {
 // TODOリストに項目を追加する
 app.post('/api/v1/add', (req, res) => {
     const todoData = req.body;
-    console.log(req.body)
+    console.log(todoData)
     const todoTitle = todoData.title;
+    console.log(todoTitle)
 
     // ユニークIDを生成する
     const id = uuidv4();
@@ -42,6 +43,25 @@ app.post('/api/v1/add', (req, res) => {
 
     // 追加した項目をクライアントに返す
     res.json(todoItem);
+});
+
+// http://localhost:3000/api/v1/item/:id にDELETEで送信してきたときに
+// 項目を削除する。:idの部分にはIDが入る
+// 例えば
+// http://localhost:3000/api/v1/item/cc7cf63c-ccaf-4401-a611-f19daec0f74e
+// にDELETEメソッドでアクセスすると、idがcc7cf63c-ccaf-4401-a611-f19daec0f74eのものが削除される
+app.delete('/api/v1/item/:id', (req, res) => {
+    // URLの:idと同じIDを持つ項目を検索
+    const index = todoList.findIndex((item) => item.id === req.params.id);
+
+    // 項目が見つかった場合
+    if(index >= 0) {
+        const deleted = todoList.splice(index, 1); // indexの位置にある項目を削除
+        console.log('Delete: ' + JSON.stringify(deleted[0]));
+    }
+
+    // ステータスコード200:OKを送信
+    res.sendStatus(200);
 });
 
 // ポート3000でサーバを立てる
